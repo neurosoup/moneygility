bf2d5e81ea45ea49274e3d54d5b98813f28cce49
neurosoup <neurosoup@gmail.com>
2014-07-08 23:10:40 +0200

    quick setup : adding operations

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -8,7 +8,7 @@ class BootStrap {
 
         //Add the default frequencies
         if (!Operation.findByLabel(Frequency.MONTHLY_CODE)) {
-            def operation = new Operation(label: Frequency.MONTHLY_CODE, frequency: Frequency.getMonthly(1))
+            def operation = new Operation(label: Frequency.MONTHLY_CODE, frequency: Frequency.getMonthly(1), amount: 0)
 
             if (!operation.save()) {
                 operation.errors.each {
