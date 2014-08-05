405b3cc2aaa915dbaa4edc3e2cd11dff13530d92
neurosoup <neurosoup@gmail.com>
2014-08-05 18:08:55 +0200

    Add operation

--- test/unit/com/moneygility/SeriesServiceSpec.groovy
+++ test/unit/com/moneygility/SeriesServiceSpec.groovy
@@ -14,20 +14,7 @@ class SeriesServiceSpec extends Specification {
 
     def setup() {
 
-        def person = new Person(firstName: "john", lastName: "doe")
-        person.save(failOnError: true)
 
-        def start = DateTime.now()
-        def end = DateTime.now().plusYears(2)
-        def plan = new Plan(label: "test plan", isActive: true, start: start, end: end)
-        plan.save(failOnError: true)
-
-        def frequency = new Frequency(code: "monthly", cronExpression: "0 0 1 5 1/1 ? *", start: start, end: end)
-
-        def series = new Series(label: "test series", frequency: frequency, plan: plan)
-
-        def operation1 = new Operation(series: series, when: DateTime.now(), amount: 0)
-        def operation2 = new Operation(series: series, when: DateTime.now().plusMonths(1), amount: 0)
     }
 
     def cleanup() {
@@ -35,7 +22,25 @@ class SeriesServiceSpec extends Specification {
 
     void "given a series with one operation in the current month and one operation next month when get operations on current month then return one operation"() {
         given:
-        def series = new Series()
+        def person = new Person(firstName: "john", lastName: "doe")
+        person.save(failOnError: true)
+
+        def start = DateTime.now()
+        def end = DateTime.now().plusYears(2)
+        def plan = new Plan(person: person, label: "test plan", isActive: true, start: start, end: end)
+        plan.save(failOnError: true)
+
+        def frequency = new Frequency(code: "monthly", cronExpression: "0 0 1 5 1/1 ? *", start: start, end: end)
+
+        def series = new Series(label: "test series", frequency: frequency, plan: plan)
+
+        series.operations.add(new Operation(series: series, when: DateTime.now(), amount: 0))
+        series.operations.add(new Operation(series: series, when: DateTime.now().plusMonths(1), amount: 0))
+
+        series.save(failOnError: true)
+
+        def sq = Series.find { it.label == "test series" }
+
         when:
         def result = service.getOperationsOfMonth(series)
 
